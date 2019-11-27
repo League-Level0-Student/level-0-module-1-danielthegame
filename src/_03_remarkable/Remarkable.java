01c2e7ef1f7530e33e481b8233c874af61d230d2
i-lab user <i-labuser@SDPL88534-iMacProL.local>
2019-11-26 17:47:39 -0800

    box msg and remarkebal finished

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -4,24 +4,32 @@ package _03_remarkable;
 import javax.swing.JOptionPane;
 
 public class Remarkable {
-private static final String JOPationPainshowInputDialogn = null;
+	private static final String JOPationPainshowInputDialogn = null;
 
-public static void main(String[] args) {	
-String matt ="taught scuba in hawaii";
-	String david="has two eyes"; 
-	String adora="has a awsome jacket";	
-	String michiyo="likes cookies"; 
-	String name=JOptionPane.showInputDialog(null,"whats your name");
-	
-	if(name.equals("adora")) {
-	JOptionPane.showMessageDialog(null,adora);
-	
+	public static void main(String[] args) {
+		String matt = "taught scuba in hawaii";
+		String david = "has two eyes";
+		String adora = "has a awsome jacket";
+		String michiyo = "likes cookies";
+		String name = JOptionPane.showInputDialog(null, "whats your name");
+
+		if (name.equals("adora")) {
+			JOptionPane.showMessageDialog(null, adora);
+
+		}
+		if (name.equals("matt")) {
+			JOptionPane.showMessageDialog(null, matt);
+		}
+
+		if (name.equals("david")) {
+			JOptionPane.showMessageDialog(null, david);
+		}
+
+		if (name.equals("michiyo")) {
+			JOptionPane.showMessageDialog(null, michiyo);
+
+		}
+
 	}
-
-
-}
-
-
-
 
 }
