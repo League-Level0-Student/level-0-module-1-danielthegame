69c5d3cafb8698de41f56a5d2f3591983b73f406
i-lab user <i-labuser@SDPL88533-iMacProLO.local>
2019-12-03 17:57:30 -0800

    finished with secretmessagebox

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -1,51 +1,44 @@
 package _04_secret_message_box;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
 /**
  * Secret Message Box / Secure Messaging System
  * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
+ * You want to leave a message on one of the Mac computers so that only your
+ * friend can read it. You set up the passcode and the secret message. Your
+ * friend types in the passcode to retrieve the message.
  * 
  */
 
 public class SecretMessageBox {
 
-	
-
 	public static void main(String[] args) {
 
 		// 0. Make a main method and put steps 1-5 inside it
-		
+
 		// 1. Set a password in a String variable
-		String pw="soccer123";
-		// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-String msg=JOptionPane.showInputDialog(null,"TELL ME A MSG");
-	
-		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-		//    if they can guess the passcode
+		String pw = "soccer123";
+		// 2. Using a pop-up, ask the first person for a secret message and store it in
+		// a variable
+		String msg = JOptionPane.showInputDialog(null, "TELL ME A SECRET MSG");
+		String pas = JOptionPane.showInputDialog(null, "tell me the pasword");
 
 		// 4. If their guess matches the password, show them the secret message
 
 		// 5. If the password does not match, pop-up "INTRUDER
-		
-			
-		
-		
-		
-		
-		
-		
-		
+
+		if (pas.equals(pw)) {
+
+			JOptionPane.showMessageDialog(null, msg);
+		} else {
+			JOptionPane.showMessageDialog(null, "INTRUDER");
+
+		}
+
 	}
-	
-	
-	
-	
-	
+
 }
