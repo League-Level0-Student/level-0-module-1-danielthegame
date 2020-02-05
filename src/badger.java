fcf69ee9c401c8ccd7f64ad28ab2bc2e07969d53
i-lab user <i-labuser@SDPL88539-iMacProLO.local>
2020-02-04 17:53:59 -0800

    badger and banana

---
+++ src/badger.java
@@ -0,0 +1,19 @@
+
+public class badger {
+	public static void main(String[] args) {
+		for (int i = 0; i < 12; i++) {
+			System.out.println("badger");
+		
+		}
+		for (int i = 0; i <2; i++) {
+			System.out.println("mushroom");
+		}
+	
+
+
+	for (int i =0; i <2; i++) {
+		System.out.println("snake");
+	}
+	}
+
+}
